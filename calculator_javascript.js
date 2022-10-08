a94fd51db8ce64719085f5d5557dd14fc6879aa1
Sampath Angara <61260495+Sampath942@users.noreply.github.com>
2022-10-08 19:09:47 +0530

    WD-3

---
+++ calculator_javascript.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('.execute').click(function (){
+    let h=$('#output').text();
+    let b=this.id;
+    let u=String(document.getElementById(b).innerText);
+        $('#output').text(h+u);
+    })
+    $('#equals').click(function() {
+    let h=$('#output').text();
+    $('#output').text(eval(h));
+    })
+    $('#cls').click(function () {
+    $('#output').text("");
+    })
+    
+    $('#backspace').click(function () {
+    let h=$('#output').text();
+    let l=h.length;
+    //alert(h);
+    h=h.substr(0,l-1);
+    //alert(h);
+    $('#output').text(h);
+    })
+    });
